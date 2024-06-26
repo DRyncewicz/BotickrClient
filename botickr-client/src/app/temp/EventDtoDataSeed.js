73356a1c7b68e6d22c2229e232128e4a0ba5b887
Dominik Ryryry <ereence@gmail.com>
2024-06-27 00:05:22 +0200

    Feat: Main conent - carousel item
    - Modified carousel item for main site content - dynamic load,
    - Modified header and search component - stylization,
    - Added data seed as temporary for testing period

---
+++ botickr-client/src/app/temp/EventDtoDataSeed.js
@@ -0,0 +1,98 @@
+const eventDto = [{
+    Id: 1,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 2,
+    Name: "KLUSKA PSIA BANDA 2024",
+    StartDate: "2024-12-12",
+    City: "WARSZAWA",
+    Description: 'Example Description for event',
+    ImagePath: "https://zooart.com.pl/blog/wp-content/uploads/2022/04/FOTO-SZPIC-NIEMIECKI-OKULARY-1000x667-1.jpg"
+},
+{
+    Id: 3,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 4,
+    Name: "KLUSKA PSIA BANDA 2024",
+    StartDate: "2024-12-12",
+    City: "WARSZAWA",
+    Description: 'Example Description for event',
+    ImagePath: "https://zooart.com.pl/blog/wp-content/uploads/2022/04/FOTO-SZPIC-NIEMIECKI-OKULARY-1000x667-1.jpg"
+},
+{
+    Id: 5,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 6,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 7,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 8,
+    Name: "KLUSKA PSIA BANDA 2024",
+    StartDate: "2024-12-12",
+    City: "WARSZAWA",
+    Description: 'Example Description for event',
+    ImagePath: "https://zooart.com.pl/blog/wp-content/uploads/2022/04/FOTO-SZPIC-NIEMIECKI-OKULARY-1000x667-1.jpg"
+},
+{
+    Id: 9,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 10,
+    Name: "KLUSKA PSIA BANDA 2024",
+    StartDate: "2024-12-12",
+    City: "WARSZAWA",
+    Description: 'Example Description for event',
+    ImagePath: "https://zooart.com.pl/blog/wp-content/uploads/2022/04/FOTO-SZPIC-NIEMIECKI-OKULARY-1000x667-1.jpg"
+},
+{
+    Id: 11,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+},
+{
+    Id: 12,
+    Name: "Young leosia BAMBI IRL 2024",
+    StartDate: "2024-12-12",
+    City: "Koszalin",
+    Description: 'Example Description for event',
+    ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
+}];
+
+export default eventDto
--- botickr-client/src/app/ui/layout-content/SearchEventComponent.js
+++ botickr-client/src/app/ui/layout-content/SearchEventComponent.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import styles from '../../styles/layout-content/SearchEventComponent.module.css';
 import SearchEventListComponent from './SearchEventListComponent';
-
+import eventDtos from '@/app/temp/EventDtoDataSeed';
 const SearchEventComponent = () => {
     const [isExpanded, setIsExpanded] = useState(false);
     const containerRef = useRef(null);
@@ -27,47 +27,37 @@ const SearchEventComponent = () => {
         };
     }, []);
 
-    const eventDto = [{
-        Id: 1,
-        Name: "Young leosia BAMBI IRL 2024",
-        StartDate: "2024-12-12",
-        City: "Koszalin",
-        ImagePath: "https://4fun.tv/uploads/media/cache/news_big/uploads/media/news/0001/25/dccad396432e386e9e58123eca1c551843a326b1.png"
-    },
-    {
-        Id: 2,
-        Name: "KLUSKA PSIA BANDA 2024",
-        StartDate: "2024-12-12",
-        City: "WARSZAWA",
-        ImagePath: "https://zooart.com.pl/blog/wp-content/uploads/2022/04/FOTO-SZPIC-NIEMIECKI-OKULARY-1000x667-1.jpg"
-    }];
+    const eventDto = eventDtos.slice(0, 4);
+
 
     return (
         <section className='search mx-5'>
             <div className='container align-items-center'>
                 <div className='d-col justify-content-center align-items-center'>
-                    <div className={styles.formContainer}> {/* Nowy kontener */}
+                    <div className={styles.formContainer}>
                         <form className='d-flex justify-content-center'>
                             <div className='col-2'>
                                 <input
-                                    className='btn btn-light form-select-lg w-100 h-100'
+                                    className='btn btn-light form-select-lg w-100 h-100 rounded-0 border border-dark'
                                     onClick={handleFocus}
+                                    placeholder='Events, Arists'
                                 />
                             </div>
                             <div className='col-2'>
                                 <input
-                                    className='btn btn-light form-select-lg w-100 h-100'
-                                    type='button'>
+                                    className='btn btn-light form-select-lg w-100 h-100 rounded-0 border border-dark'
+                                    placeholder='Date'>
+                                        
                                 </input>
                             </div>
                             <div className='col-2'>
                                 <input
-                                    className='btn btn-light form-select-lg w-100 h-100'
-                                    type='button'>
+                                    className='btn btn-light form-select-lg w-100 h-100 rounded-0 border border-dark'
+                                    placeholder='Localization'>
                                 </input>
                             </div>
                             <div className='col-2'>
-                                <button className='btn btn-dark btn-lg justify-content-center w-100'>
+                                <button className='btn btn-dark btn-lg justify-content-center w-100 rounded-0'>
                                     <i className="bi bi-search-heart-fill"></i>
                                     SEARCH
                                 </button>
